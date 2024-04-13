7c444b8672891581bce77dcc0a80bca83470fa04
Jopus <jopus@Jopusdebijibendiannao.local>
2024-04-13 16:08:37 +0800

    release

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     'title': 'Jopus Manual', // 站点标题
     'pages': {
         //'Articles': '9f31ea21-90b9-4523-b8d5-cb33b7a01bda',
-        'Blog': 'https://jopus.cn',
+        //'Blog': 'https://jopus.cn',
         // 'Activity':'activity', // 花园活跃状态页面
         'Readme': '6d30fd00-2557-49f7-9595-21d6dff24e7d'
     },
