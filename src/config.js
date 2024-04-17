08347f57ea4cc4393a524781eff0dabf6ad5baf6
Jopus <jopus@Jopusdendiannao.lan>
2024-04-17 21:15:56 +0800

    release

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     'pages': {
         'About': '6d30fd00-2557-49f7-9595-21d6dff24e7d',
         // 'Activity':'activity', // 花园活跃状态页面
-        'ReadMe': '85122c3e-0d6c-488c-bfb3-02081838bd31'
+        'Readme': '85122c3e-0d6c-488c-bfb3-02081838bd31'
     },
     'server': '', // Discord 服务器 ID，非必填，填写后将在网站中显示聊天入口
     'channel': '' // Discord 频道 ID，非必填，填写后将在网站中显示聊天入口
